d4568500b28efe33a1134ab4531c9d296cc081a9
emustafaa <edward.mustafaa@yahoo.com>
2021-03-23 20:42:19 -0500

    Added driving mechanic

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float cameraOffsetX;
+    public float cameraOffsetY = 10.0f;
+    public float cameraOffsetZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        cameraOffsetX = target.position.x;
+        cameraOffsetZ = target.position.z;
+        transform.position = new Vector3(target.position.x, cameraOffsetY, target.position.z);
+    }
+}
---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float verticalInput;
+    private float currentBrakeForce;
+    private float currentSteeringAngle;
+
+    private bool isBraking;
+    private bool changeCamera;
+    
+
+    [SerializeField] private float motorForce;
+    [SerializeField] private float brakeForce;
+    [SerializeField] private float maxSteeringAngle;
+
+    [SerializeField] private WheelCollider frontDriverWheelCollider;
+    [SerializeField] private WheelCollider frontPassengerWheelCollider;
+    [SerializeField] private WheelCollider rearDriverWheelCollider;
+    [SerializeField] private WheelCollider rearPassengerWheelCollider;
+
+    [SerializeField] private Transform frontDriverWheelTransform;
+    [SerializeField] private Transform frontPassengerWheelTransform;
+    [SerializeField] private Transform rearDriverWheelTransform;
+    [SerializeField] private Transform rearPassengerWheelTransform;
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+    }
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        isBraking = Input.GetKey(KeyCode.Space);
+        changeCamera = Input.GetKey(KeyCode.Return);
+
+    }
+
+    private void HandleMotor()
+    {
+        frontDriverWheelCollider.motorTorque = verticalInput * motorForce;
+        frontPassengerWheelCollider.motorTorque = verticalInput * motorForce;
+        currentBrakeForce = isBraking ? brakeForce : 0f;
+        ApplyBrakes();
+
+    }
+
+    private void ApplyBrakes()
+    {
+        frontDriverWheelCollider.brakeTorque = currentBrakeForce;
+        frontPassengerWheelCollider.brakeTorque = currentBrakeForce;
+        rearDriverWheelCollider.brakeTorque = currentBrakeForce;
+        rearPassengerWheelCollider.brakeTorque = currentBrakeForce;
+    }
+
+    private void HandleSteering()
+    {
+        currentSteeringAngle = maxSteeringAngle * horizontalInput;
+        frontDriverWheelCollider.steerAngle = currentSteeringAngle;
+        frontPassengerWheelCollider.steerAngle = currentSteeringAngle;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateWheels(frontDriverWheelCollider, frontDriverWheelTransform);
+        UpdateWheels(frontPassengerWheelCollider, frontPassengerWheelTransform);
+        UpdateWheels(rearDriverWheelCollider, rearDriverWheelTransform);
+        UpdateWheels(rearPassengerWheelCollider, rearPassengerWheelTransform);
+        
+    }
+
+    private void UpdateWheels(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
+    }
+}
